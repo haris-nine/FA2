62d84ce18ca405faee046a1f3e4261c26778fa48
harry <87892644+haris-nine@users.noreply.github.com>
2024-11-05 14:02:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -102,3 +102,19 @@ window.transitionToPage = function(href) {
 document.addEventListener('DOMContentLoaded', function(event) {
   document.querySelector('body').style.opacity = 1
 })
+
+var links = document.querySelectorAll('a');
+
+[].forEach.call(links, function(link) {
+    
+  link.addEventListener("mouseenter", function() {
+    
+    var newPreLoadLink = document.createElement("link");
+    newPreLoadLink.rel = "prerender";
+    newPreLoadLink.href = link.href;
+    
+    document.head.appendChild(newPreLoadLink);
+    
+  })
+  
+});
