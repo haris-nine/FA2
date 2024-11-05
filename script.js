45cd72b0eda49a4b2b5d5e3451530b7fe11621d1
harry <87892644+haris-nine@users.noreply.github.com>
2024-11-05 14:11:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -91,18 +91,6 @@ function removeContent() {
 }
 
 
-
-window.transitionToPage = function(href) {
-  document.querySelector('body').style.opacity = 0
-  setTimeout(function() { 
-      window.location.href = href
-  }, 500)
-}
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  document.querySelector('body').style.opacity = 1
-})
-
 var links = document.querySelectorAll('a');
 
 [].forEach.call(links, function(link) {
